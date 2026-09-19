f54da52af971f22613c2e5a93a6327734d013b3a
celeste barros <celestebarros09@gmail.com>
2026-09-19 19:48:58 -0300

    hice el primer borrador del juego de trivia

---
+++ script.js
@@ -0,0 +1,27 @@
+async function probarFetch() {
+  const respuesta = await fetch("https://opentdb.com/api.php?amount=10&category=25&difficulty=easy&type=multiple");
+  const datos = await respuesta.json();
+  console.log(datos);
+
+  const primeraPregunta = datos.results[0];
+  document.getElementById("pregunta").textContent = primeraPregunta.question;
+
+  const todasLasOpciones = [primeraPregunta.correct_answer, ...primeraPregunta.incorrect_answers];
+  const contenedorOpciones = document.getElementById("opciones");
+
+  todasLasOpciones.forEach(function(opcion) {
+    const boton = document.createElement("button");
+    boton.textContent = opcion;
+
+    boton.addEventListener("click", function() {
+  if (opcion === primeraPregunta.correct_answer) {
+    document.getElementById("resultado").textContent = "¡Correcto!";
+  } else {
+    document.getElementById("resultado").textContent = "Incorrecto. La respuesta correcta es: " + primeraPregunta.correct_answer;
+  }
+});
+    contenedorOpciones.appendChild(boton);
+  });
+}
+
+probarFetch();
